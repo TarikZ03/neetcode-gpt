2c8b627bb20c9c0a5f867c32ea5b9c160851e983
Tarik Začiragić <144945507+TarikZ03@users.noreply.github.com>
2026-06-04 15:40:23 +0200

    Implement PyTorch basics (PyTorch 1/4)

---
+++ foundations/pytorch_basics.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn
+from torchtyping import TensorType
+
+# Round all answers to 4 decimal places: torch.round(tensor, decimals=4)
+class Solution:
+    def reshape(self, to_reshape: TensorType[float]) -> TensorType[float]:
+        # Reshape (M, N) tensor to (M*N/2, 2)
+        # Use torch.reshape(tensor, new_shape)
+
+        #to_reshape.shape[0] grabs the 0th dim from to_shape matrix, i.e., M
+        return torch.reshape(to_reshape, ((to_reshape.shape[0]*to_reshape.shape[1])//2, 2))
+
+    def average(self, to_avg: TensorType[float]) -> TensorType[float]:
+        # Compute column-wise mean (average across rows)
+        # Use torch.mean(tensor, dim=0)
+        
+        #dim removes that dim so if we want to do mean across rows we need to remove the rows, aka squish n rows into 1, so column-wise mean you will have 1 row and i number of elements where i=the number of columns 
+        return torch.mean(to_avg, dim=0)
+
+    def concatenate(self, cat_one: TensorType[float], cat_two: TensorType[float]) -> TensorType[float]:
+        # Join two tensors side-by-side along dim=1
+        # Use torch.cat((a, b), dim=1)
+        
+        #dim removes that dimension, so that can be joined together
+        return torch.cat((cat_one, cat_two), dim=1)
+
+    def get_loss(self, prediction: TensorType[float], target: TensorType[float]) -> TensorType[float]:
+        # Compute Mean Squared Error between prediction and target
+        # Use torch.nn.functional.mse_loss(prediction, target)
+        
+        return torch.nn.functional.mse_loss(prediction, target)
